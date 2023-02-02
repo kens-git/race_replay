71353c5e3c3423c1b326ab6223a8af821572ccee
Ken Montgomery <>
2023-02-02 12:19:47 -0500

    Initial setup.

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+      return <p>Just an empty page.</p>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.[fullhash].js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {static: './dist',},
+    module: {
+        rules: [
+        {
+            test: /\.(js|ts)x?$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"]
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: ["style-loader", "css-loader"]
+        },
+        {
+            test: /\.(png|svg|jpg|gif)$/,
+            exclude: /node_modules/,
+            use: ["file-loader"]
+        }, 
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.tsx', '.ts', '.jsx', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        }
+    },
+    plugins: [new HtmlWebpackPlugin({template: './public/index.html'})]
+}
